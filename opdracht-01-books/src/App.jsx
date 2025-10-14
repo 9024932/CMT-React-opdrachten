78d0361835f3c402c32ab0c4d0f12660f85b9191
Juliany Rosenblad <9024932@student.zadkine.nl>
2025-10-14 10:53:34 +0200

    Opdracht 1.1: Uitbreiding Books

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import BookList from './assets/components/BookList';
+import Header from './assets/components/Header';
  
 function App() {
  
   return (
     <>
+    <Header/>
     <BookList/>
       </>
   );
--- opdracht-01-books/src/assets/components/BookList.jsx
+++ opdracht-01-books/src/assets/components/BookList.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import Book from './Book';
 const BookList = () => {
   return (
