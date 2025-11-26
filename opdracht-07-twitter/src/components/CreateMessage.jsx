dbfc52f70238616d05f59ab2012e4cb035c0b5d6
Juliany Rosenblad <9024932@student.zadkine.nl>
2025-11-26 17:12:19 +0100

    twitter

---
+++ opdracht-07-twitter/src/components/CreateMessage.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+const CreateMessage = ({textInput, setTextInput, message, setMessage }) => {
+
+const userInputHandler = (e) => {
+    setTextInput(e.target.value);
+}
+
+const SubmitMessageHandler = (e) => {
+    e.preventDefault();
+
+    setMessage([...message, textInput])
+    setTextInput('');
+}
+
+    return ( 
+        <form action="">
+            <textarea value={textInput} cols="50" rows="5" onChange={userInputHandler}></textarea>
+            <button onClick={SubmitMessageHandler} >Toevoegen</button>
+        </form>
+     );
+}
+ 
+export default CreateMessage;
---
+++ opdracht-07-twitter/src/components/Message.jsx
@@ -0,0 +1,9 @@
+const Message = ({name, message}) => {
+    return ( <div>
+        <h2>{name}</h2>
+        <p>{message}</p>
+    </div>
+     );
+}
+ 
+export default Message;
